38bf662e21ba66f171a099494fbd91105401b689
OlgaSadokierska <115590776+OlgaSadokierska@users.noreply.github.com>
2024-05-01 19:10:17 +0200

    Add files via upload

---
+++ DreamTravel/src/main/java/com/example/DreamTravel/DreamTravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.DreamTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamTravelApplication.class, args);
+	}
+
+}
