16b94ccd2372fb13ae43da58ee73e847d587e7fb
Olga <olga.sadokierska@op.pl>
2024-05-29 13:58:05 +0200

    insert fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -38,146 +38,146 @@ VALUES (
 
 INSERT INTO
     travel (
-        country,
-        city,
-        start_date,
-        end_date,
-        description,
-        rate,
-        user_id
-    )
+    country,
+    city,
+    start_date,
+    end_date,
+    description,
+    rate,
+    user_id
+)
 VALUES (
-        'Spain',
-        'Barcelona',
-        '2024-06-01',
-        '2024-06-10',
-        'A beautiful trip to Barcelona',
-        5,
-        1
-    ),
-    (
-        'France',
-        'Paris',
-        '2024-07-15',
-        '2024-07-20',
-        'Visiting the Eiffel Tower and other attractions in Paris',
-        4,
-        2
-    ),
-    (
-        'Italy',
-        'Rome',
-        '2024-08-05',
-        '2024-08-12',
-        'Exploring the historical sites of Rome',
-        5,
-        1
-    ),
-    (
-        'Japan',
-        'Tokyo',
-        '2024-09-10',
-        '2024-09-17',
-        'Experiencing the culture and technology in Tokyo',
-        4,
-        5
-    ),
-    (
-        'Australia',
-        'Sydney',
-        '2024-10-20',
-        '2024-10-30',
-        'Enjoying the beaches and landmarks in Sydney',
-        5,
-        5
-    ),
-    (
-        'Germany',
-        'Berlin',
-        '2024-06-20',
-        '2024-06-30',
-        'Exploring the rich history and culture of Berlin',
-        5,
-        6
-    ),
-    (
-        'Canada',
-        'Toronto',
-        '2024-07-10',
-        '2024-07-18',
-        'Visiting the CN Tower and other attractions in Toronto',
-        4,
-        7
-    ),
-    (
-        'Brazil',
-        'Rio de Janeiro',
-        '2024-08-01',
-        '2024-08-10',
-        'Enjoying the beaches and carnival in Rio de Janeiro',
-        5,
-        3
-    ),
-    (
-        'South Africa',
-        'Cape Town',
-        '2024-09-05',
-        '2024-09-12',
-        'Exploring the natural beauty and landmarks of Cape Town',
-        5,
-        6
-    ),
-    (
-        'Thailand',
-        'Bangkok',
-        '2024-10-01',
-        '2024-10-08',
-        'Experiencing the vibrant city life and temples in Bangkok',
-        4,
-        5
-    ),
-    (
-        'New Zealand',
-        'Auckland',
-        '2024-11-15',
-        '2024-11-25',
-        'Exploring the stunning landscapes and culture of Auckland',
-        5,
-        7
-    ),
-    (
-        'Netherlands',
-        'Amsterdam',
-        '2024-12-01',
-        '2024-12-10',
-        'Cruising the canals and visiting museums in Amsterdam',
-        5,
-        6
-    ),
-    (
-        'Switzerland',
-        'Zurich',
-        '2024-12-15',
-        '2024-12-22',
-        'Enjoying the winter scenery and chocolate in Zurich',
-        4,
-        7
-    ),
-    (
-        'Greece',
-        'Athens',
-        '2024-12-25',
-        '2025-01-05',
-        'Exploring the ancient ruins and history of Athens',
-        5,
-        7
-    ),
-    (
-        'Turkey',
-        'Istanbul',
-        '2025-01-10',
-        '2025-01-20',
-        'Experiencing the cultural blend and landmarks in Istanbul',
-        5,
-        6
-    );
+           'Hiszpania',
+           'Barcelona',
+           '2024-06-01',
+           '2024-06-10',
+           'Piękna podróż do Barcelony, w której zwiedzamy słynne zabytki, cieszymy się słońcem na plażach oraz kosztujemy pyszne lokalne jedzenie.',
+           5,
+           1
+       ),
+       (
+           'Francja',
+           'Paryż',
+           '2024-07-15',
+           '2024-07-20',
+           'Odwiedzamy Wieżę Eiffla oraz inne atrakcje turystyczne w Paryżu, spacerujemy po urokliwych uliczkach oraz delektujemy się francuskimi przysmakami.',
+           4,
+           2
+       ),
+       (
+           'Włochy',
+           'Rzym',
+           '2024-08-05',
+           '2024-08-12',
+           'Odkrywamy historyczne miejsca Rzymu, takie jak Koloseum i Forum Romanum, oraz smakujemy autentyczne włoskie jedzenie.',
+           5,
+           1
+       ),
+       (
+           'Japonia',
+           'Tokio',
+           '2024-09-10',
+           '2024-09-17',
+           'Doświadczamy kultury i technologii w Tokio, odwiedzamy świątynie, uczestniczymy w festiwalach oraz podziwiamy nowoczesne dzielnice.',
+           4,
+           5
+       ),
+       (
+           'Australia',
+           'Sydney',
+           '2024-10-20',
+           '2024-10-30',
+           'Cieszymy się plażami i zabytkami w Sydney, w tym Operą w Sydney i mostem Harbour Bridge, a także odkrywamy australijską przyrodę.',
+           5,
+           5
+       ),
+       (
+           'Niemcy',
+           'Berlin',
+           '2024-06-20',
+           '2024-06-30',
+           'Odkrywamy bogatą historię i kulturę Berlina, zwiedzamy Mury Berlińskie, Muzeum Pergamońskie oraz cieszymy się tętniącym życiem miasta.',
+           5,
+           6
+       ),
+       (
+           'Kanada',
+           'Toronto',
+           '2024-07-10',
+           '2024-07-18',
+           'Odwiedzamy wieżę CN i inne atrakcje w Toronto, spacerujemy po mieście, odwiedzamy wyspy torontońskie i delektujemy się kanadyjską kuchnią.',
+           4,
+           7
+       ),
+       (
+           'Brazylia',
+           'Rio de Janeiro',
+           '2024-08-01',
+           '2024-08-10',
+           'Cieszymy się plażami i karnawałem w Rio de Janeiro, odwiedzamy słynną statuę Chrystusa Odkupiciela oraz podziwiamy piękne widoki z Głowy Cukru.',
+           5,
+           3
+       ),
+       (
+           'Republika Południowej Afryki',
+           'Kapsztad',
+           '2024-09-05',
+           '2024-09-12',
+           'Odkrywamy naturalne piękno i zabytki Kapsztadu, takie jak Góra Stołowa, Robben Island i ogrody botaniczne Kirstenbosch.',
+           5,
+           6
+       ),
+       (
+           'Tajlandia',
+           'Bangkok',
+           '2024-10-01',
+           '2024-10-08',
+           'Doświadczamy życia w tętniącym życiem mieście oraz odwiedzamy świątynie w Bangkoku, kosztujemy tajskiej kuchni oraz odkrywamy lokalne rynki.',
+           4,
+           5
+       ),
+       (
+           'Nowa Zelandia',
+           'Auckland',
+           '2024-11-15',
+           '2024-11-25',
+           'Odkrywamy oszałamiające krajobrazy i kulturę Auckland, zwiedzamy malownicze wyspy, parki narodowe oraz próbujemy nowozelandzkich win.',
+           5,
+           7
+       ),
+       (
+           'Holandia',
+           'Amsterdam',
+           '2024-12-01',
+           '2024-12-10',
+           'Pływamy po kanałach oraz odwiedzamy muzea w Amsterdamie, takie jak Rijksmuseum i Muzeum van Gogha, spacerujemy po urokliwych uliczkach miasta.',
+           5,
+           6
+       ),
+       (
+           'Szwajcaria',
+           'Zurych',
+           '2024-12-15',
+           '2024-12-22',
+           'Cieszymy się zimowymi krajobrazami i czekoladą w Zurychu, zwiedzamy stare miasto oraz korzystamy z uroków alpejskiego regionu.',
+           4,
+           7
+       ),
+       (
+           'Grecja',
+           'Ateny',
+           '2024-12-25',
+           '2025-01-05',
+           'Odkrywamy starożytne ruiny i historię Aten, odwiedzamy Akropol, Partenon oraz kosztujemy tradycyjnej greckiej kuchni.',
+           5,
+           7
+       ),
+       (
+           'Turcja',
+           'Stambuł',
+           '2025-01-10',
+           '2025-01-20',
+           'Doświadczamy kulturalnej mieszanki oraz zabytków Stambułu, zwiedzamy Hagia Sophia, Błękitny Meczet oraz lokalne bazary.',
+           5,
+           6
+       );
